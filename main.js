29a322aabd8013793e85b44a802cc589158878e6
Logan Swanson <logswan9@gmail.com>
2022-08-01 23:18:42 -0500

    Reworked main.js. Able to open window for user input

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-const { app, BrowserWindow } = require('electron')
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+const ipc = electron.ipcMain;
 
+let mainWindow;
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 1200,
-      height: 1000,
-      icon: 'src/images/cat.png',
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false
-    }
-    })
-  
-      
-
-    win.loadFile('index.html')
-  }
-
-app.whenReady().then(() => {
-createWindow()
-})
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 1000,
+    icon: 'src/images/cat.png',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    resizable: false
+  });
+  mainWindow.loadFile('index.html')
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
+
+
+
+let inputWindow;
+
+ipc.on('get-input', function () {
+  inputWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    icon: 'src/images/cat.png',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    resizable: false
+  });
+  inputWindow.loadFile('test.html')
+});
+
+
